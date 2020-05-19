a263e16ed5d0be4783805420540980c99731856b
chenguoqing <chenguoqing@zhuojianchina.com>
2020-05-19 21:55:02 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,10 @@
     <div class='app-wrapper'>
         <span>app运行成功哈哈哈哈</span>
         <div>风格化风格化风格规划规范化化445</div>
+        <div class="box">
+            <div>等分效果</div>
+            <div>等分效果</div>
+        </div>
     </div>
 </template>
 
@@ -12,25 +16,32 @@ export default {
     components: {},
 
     data() {
-        return {
-            
-        };
+        return {};
     },
 
     methods: {}
 };
 </script>
 <style lang='scss' scoped>
-.app-wrapper{
+.app-wrapper {
     height: 1000px;
-    background-image: url('./assets/images/veer-141542994.jpg');
-    span{
+    // background-image: url("./assets/images/veer-141542994.jpg");
+    span {
         font-size: 18px;
         color: orange;
         font-weight: bold;
     }
-    div{
+    div {
         box-shadow: 0 0 0 #000;
+    }
+    .box {
+        display: flex;
+        div {
+            height: 300px;
+            border: 1px solid #000000;
+            flex: 1; //这就是flex：1的妙用
+            text-align: center;
+        }
     }
 }
 </style>
