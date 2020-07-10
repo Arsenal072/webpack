3646702ba63853a0fb48a5d6acb8cb76be135a24
chenguoqing <chenguoqing@zhuojianchina.com>
2020-07-10 21:43:44 +0800

    update

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -8,34 +8,34 @@ const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 module.exports = WebpackMerge(webpackConfig, {
     mode: 'production',
-    devtool: 'cheap-module-eval',
+    devtool: 'cheap-module',
     plugins: [
         // new BundleAnalyzerPlugin(),
-        // new ParallelUglifyPlugin({
-        //     // cacheDir: path.resolve(__dirname, '../dist/cache'),
-        //     sourceMap: false,
-        //     uglifyJS: {
-        //         output: {
-        //             comments: false,
-        //             beautify: false
-        //         },
-        //         warnings: false,
-        //         compress: {
-        //             drop_console: true,
-        //             collapse_vars: false,
-        //             reduce_vars: true,
-        //         }
-        //     }
-        // }),
+        new ParallelUglifyPlugin({
+            // cacheDir: path.resolve(__dirname, '../dist/cache'),
+            sourceMap: false,
+            uglifyJS: {
+                output: {
+                    comments: false,
+                    beautify: false
+                },
+                warnings: false,
+                compress: {
+                    drop_console: true,
+                    collapse_vars: false,
+                    reduce_vars: true,
+                }
+            }
+        }),
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano')
         }),
-        // new CompressionPlugin({
-        //     test: /\.js$|\.html$|.\css/,
-        //     threshold: 10240, //对超过10k的数据压缩
-        //     deleteOriginalAssets: false //删除源文件
-        // })
+        new CompressionPlugin({
+            test: /\.js$|\.html$|.\css/,
+            threshold: 10240, //对超过10k的数据压缩
+            deleteOriginalAssets: false //删除源文件
+        })
         // new CopyWebapckPlugin([{
         //     from: path.resolve(__dirname, '../public'),
         //     to: path.resolve(__dirname, '../dist')
